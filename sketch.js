333d9b4dc2f0588220002e5ced6c460c73c62834
grazielstokosa <160882314+grazielstokosa@users.noreply.github.com>
2024-09-26 09:25:15 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// fantasia, aventura, drama,ação
+
+// velozes e furiosos, 13, ação, aventura
+// guardiões da galáxia, 12, fantasia, aventura
+// deadpool, 15, drama, fantasia, aventura
+// shrek, LIVRE, fantasia, aventura
+// ladrões de bicicleta, 12, drama
+// o menino que descobriu o vento, 14, drama
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
